f74402aeb2f5cd33dee1858a47c078a9bfe41c07
deeppomal <deeppomal98@gmail.com>
2022-12-23 00:37:12 -0500

    bug fix

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -12,7 +12,7 @@ export const HomeScreen = () => {
       <div className='flex w-full h-full flex-col-reverse lg:flex-row'>
         <div className='h-1/3 lg:h-full lg:w-1/2 flex lg:justify-center lg:items-end items-center flex-col '>
           <div className='flex flex-col '>
-            <p className='lg:text-3xl md:text-2xl text-xl text-[#f6f6f6] font-Montserrat '>Hi, I'm<span className='text-[#f5c169] lg:ml-4 text-4xl lg:text-7xl font-Ubuntu'> Deep Pomal</span></p>
+            <p className='lg:text-3xl md:text-2xl text-xl text-[#f6f6f6] font-Montserrat '>Hi, I&apos;m<span className='text-[#f5c169] lg:ml-4 text-4xl lg:text-7xl font-Ubuntu'> Deep Pomal</span></p>
             <p className='lg:text-3xl md:text-2xl text-xl text-[#f6f6f6] mt-4 font-Montserrat'>Full Stack Developer</p>
             <a href="https://drive.google.com/file/d/1QVe3IHNqYO1ZsVS_4vIVExd5MItxWgzo/view?usp=sharing" target={'_blank'} rel="noreferrer" className='lg:w-56'>
               <div className='lg:border-4 border-2 border-[#f5c169] lg:w-56 py-2 rounded flex items-center justify-center mt-12 cursor-pointer 
