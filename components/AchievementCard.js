e15a8ac3dc95b7f81f46148d9935d03ba2bd4130
deeppomal <deeppomal98@gmail.com>
2023-08-17 01:04:18 -0400

    achievement car bug fix

--- components/AchievementCard.js
+++ components/AchievementCard.js
@@ -5,8 +5,7 @@ export const  AchievementCard = ({data,index}) => {
   return (
     <div className={`w-full flex flex-col items-center justify-center border-y border-[#14303f] dark:border-[#ffb39b] ${index == 1 && 'lg:border-l'}`}>
          <div 
-          height={1000} 
-          className={`bg-[url('/static/${data.image}')] my-10 lg:h-80 h-40 w-2/3 bg-cover bg-no-repeat`}/>
+          className={`bg-[url('/static/${data.image}')] my-10 lg:h-72 h-40 w-2/3 bg-cover bg-no-repeat`}/>
         <div className='w-full relative bottom-0 p-4 bg-[#d6d9df] dark:bg-[#122935] flex items-center justify-center'>
           <a href={data.link}><p className='text-lg font-Montserrat dark:text-[#f6f6f6] text-[#14303f] font-semibold cursor-pointer underline'>{data.title}</p></a>
         </div>
